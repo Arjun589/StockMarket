8bb620210019597afbbdfda133a71152f3e08dfd
Arjun589 <58413630+Arjun589@users.noreply.github.com>
2021-12-01 11:51:14 +0530

    Add files via upload

---
+++ bakend/StockPriceApplication/src/test/java/com/arjun/stockprice/application/StockPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.stockprice.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
